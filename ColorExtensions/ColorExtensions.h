81453de1e3de3f896ba05452b7f3b0e47ea7ecd1
Martin Jacob Rehder <gitrepocon01@rehsco.com>
2021-05-10 11:23:01 +0200

    Initial Commit

---
+++ ColorExtensions/ColorExtensions.h
@@ -0,0 +1,18 @@
+//
+//  ColorExtensions.h
+//  ColorExtensions
+//
+//  Created by Martin Rehder on 10.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ColorExtensions.
+FOUNDATION_EXPORT double ColorExtensionsVersionNumber;
+
+//! Project version string for ColorExtensions.
+FOUNDATION_EXPORT const unsigned char ColorExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorExtensions/PublicHeader.h>
+
+
